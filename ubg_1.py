481bde989993a2698f6c05c3dc8fdc306d6d98d8
Lennart Lehmann <lennart.lehmann@outlook.com>
2018-10-21 20:21:57 +0200

    init

---
+++ ubg_1.py
@@ -0,0 +1,105 @@
+import sys
+import copy
+from collections import Counter
+from string import ascii_lowercase
+
+CIPHER_LETTERS = 'nzghqkcdmyfoialxevtswrupjb'
+ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+ALPHADICT = {x:0 for x in ALPHABET}
+BY_FREQ = " etaoinshrdlcumwfgypbvkjxqz"
+
+def main():
+    fl = list(promptUserAndGetCipheredText())
+
+    # determine frequency of each letter in the file
+    freq_dict = letter_freq_file(fl)
+
+    #create alphabet sorted by frequency of occurence in the file.
+    alphabet_by_freq = sorted(freq_dict,
+                                key = lambda x: freq_dict[x],
+                                reverse=True)
+
+    # print substituted version of the file
+    for line in fl:
+        print(substitute(line, alphabet_by_freq))
+
+def promptUserAndGetCipheredText():
+    ciphertextFile = open('ciphertext.txt', 'w')
+    textToCipher = input('Text to cipher: ')
+  
+    if not textToCipher:
+      plaintextFile = open('plaintext.txt', 'r')
+      textToCipher = plaintextFile.read()
+    
+    ciphered = cipher(textToCipher)
+    ciphertextFile.write(ciphered)
+    print('Ciphered text saved to file.')
+    
+    return ciphered
+
+def letter_freq(s, freq_dict):
+    for letter in s:
+        if letter.lower() in ALPHABET:
+            freq_dict[letter.lower()] += 1
+
+# takes a list of strings (usually lines from a file) and returns a dict
+# containing the frequency of every character
+def letter_freq_file(string_list):
+    freq_dict = copy.deepcopy(ALPHADICT)
+    for s in string_list:
+        letter_freq(s, freq_dict)
+
+    return freq_dict
+
+def alpha_position(letter):
+    return ALPHABET.index(letter.lower()) + 1
+
+def substitute(string, alphabet_by_freq):
+    ret = ''
+    for letter in string:
+        if letter.lower() in ALPHABET:
+            ret += alphabet_by_freq[alpha_position(letter)-1]
+        else:
+            ret += letter
+    return ret
+
+def is_word(s):
+    return True
+    # test whether a string is in a dictionary.
+
+def is_decoded(sl):
+    new_sl = []
+    for s in sl:
+        s = s.strip().split(" ")
+        new_sl.append(s)
+
+    # new_sl is now a list of lists of words
+    word_count = 0
+    for i in range(10):
+        if is_word(random.choice(random.choice(new_sl))):
+            word_count += 1
+
+    if word_count >= 3: #TODO find a good number to require to be in dictionary. Inherently involves a degree of uncertainty
+        return True
+    else:
+        return False
+
+def ngrams(word, n):
+    if n>len(word):
+        return
+    for i in range(len(word)-n+1):
+        yield word[i:i+n]
+
+def count_ngrams(s, n):
+    counter = Counter()
+    for word in s.split():
+        for ngram in ngrams(word, n):
+            counter[ngram] += 1
+    return counter
+    
+def cipher(text):
+    trans = str.maketrans(ascii_lowercase, CIPHER_LETTERS)
+    return text.translate(trans)
+    
+
+main()
